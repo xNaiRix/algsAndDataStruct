0ac1fedb57f25120437a9c5d42781106aae91043
xNaiRix <nertuzer4@gmail.com>
2025-01-03 23:45:21 +0300

    Create priority_queue.py

---
+++ priority_queue.py
@@ -0,0 +1,66 @@
+def cmp(x, y):
+    return x < y
+class Heap:
+    def __init__(self, myArr = None, compare = cmp):
+        if myArr == None:
+            self.arr = list()
+            self.len = 0
+            self.compare = compare
+            return
+        self.len = len(myArr)
+        self.arr = myArr
+        self.compare = compare
+        for i in range(self.len - 1, -1, -1):
+            self.down(i)
+    def up(self, id = None):
+        if id == None:
+            id = self.len - 1
+        while (id - 1)//2 >= 0 and  self.compare(self.arr[id], self.arr[(id - 1)//2]):
+            self.arr[(id - 1)//2], self.arr[id] = self.arr[id], self.arr[(id - 1)//2]
+            id = (id - 1)//2
+    def down(self, id = None):
+        if id == None:
+            id = 0
+        while id*2 + 1 < self.len and self.compare(self.arr[2*id + 1], self.arr[id]) or id*2 + 2 < self.len and self.compare(self.arr[2*id + 2], self.arr[id]):
+            if id*2 + 2 < self.len:
+                if self.compare(self.arr[2*id + 1], self.arr[2*id + 2]) and self.compare(self.arr[2* id + 1], self.arr[id]):
+                    self.arr[id], self.arr[2*id + 1] = self.arr[2*id + 1], self.arr[id]
+                    id = 2*id + 1
+                elif self.compare(self.arr[2*id + 2], self.arr[2*id + 1]) and self.compare(self.arr[2* id + 2], self.arr[id]):
+                    self.arr[id], self.arr[2*id + 2] = self.arr[2*id + 2], self.arr[id]
+                    id = 2*id + 2
+                continue
+            if id*2 + 1 < self.len and self.compare(self.arr[2*id + 1], self.arr[id]):
+                self.arr[2*id + 1], self.arr[id] = self.arr[id], self.arr[2*id + 1]
+                id = 2*id + 1
+    def add(self, new_el):
+        self.arr.append(new_el)
+        self.len += 1
+        self.up()
+    def get_min(self):
+        if self.len == 0:
+            return None
+        return self.arr[0]
+    def pop(self):
+        if self.len == 0:
+            raise IndexError
+        first = self.arr[0]
+        self.arr[0], self.arr[-1] = self.arr[-1], self.arr[0]
+        self.arr.pop()
+        self.len -=1 
+        self.down()
+        return first
+arr = Heap(myArr = [3, 1, 3, 51, 2, 5, 2])
+for i in range(arr.len):
+    print(arr.arr[i])
+arr.add(-1)
+arr.add(10)
+arr.add(-50)
+print(arr.get_min())
+
+
+        
+
+
+
+
